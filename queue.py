29a28813f8be6c8407625dcb44abd6ff8e3337d6
Timur Hukmatov <timurhukmatov@gmail.com>
2023-01-12 21:37:33 +0300

    [add] equeue

---
+++ queue.py
@@ -0,0 +1,39 @@
+from node import Node
+
+
+class Queue:
+    def __init__(self):
+        self.head = None
+
+    def enqueue(self, data):
+        new = Node(data)
+        if not self.head:
+            self.head = new
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new
+
+    def dequeue(self):
+        last = self.head
+        self.head = last.next
+        del last
+
+    def __str__(self):
+        last = self.head
+        result = '['
+        while last:
+            result += f'{last.data}, '
+            last = last.next
+        return result[:-2] + ']'
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    print(queue)
+    queue.dequeue()
+    print(queue)
+
