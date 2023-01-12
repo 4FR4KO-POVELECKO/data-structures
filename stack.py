11df89a3992da6563bbe7116623632027823f375
Timur Hukmatov <timurhukmatov@gmail.com>
2023-01-12 21:07:18 +0300

    Stack

---
+++ stack.py
@@ -0,0 +1,34 @@
+from node import Node
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new = Node(data)
+        new.next = self.head
+        self.head = new
+
+    def pop(self):
+        self.head = self.head.next
+
+    def pip(self):
+        last = self.head
+        result = '['
+        while last:
+            result += f'{last.data}, '
+            last = last.next
+        return result[:-2] + ']'
+
+    def __str__(self):
+        return self.pip()
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    print(stack.pip())
+    stack.pop()
+    print(stack)
