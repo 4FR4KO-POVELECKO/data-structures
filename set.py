6a1024c6a63fe1b9fe177bc60554460e7c6fc6dc
Timur Hukmatov <timurhukmatov@gmail.com>
2023-01-18 23:14:55 +0300

    [refact] set from array

--- set.py
+++ set.py
@@ -1,63 +1,34 @@
-from linked_list import LinkedList
+from arr import Array
 
 
-class Set(LinkedList):
+class Set(Array):
     def add(self, data):
-        if self.search(data):
+        if data in self:
             return
-        super().add(data)
+        self.append(data)
     
     def union(self, other):
         new = Set()
-        first = self.head
-        while first:
-            new.add(first.data)
-            first = first.next
-        second = other.head
-        while second:
-            new.add(second.data)
-            second = second.next
-        
+        for i in self:
+            new.add(i)
+        for i in other:
+            new.add(i)
+
         return new
 
     def intersection(self, other):
         new = Set()
-        first = self.head
-        while first:
-            if other.search(first.data) is not None:
-                new.add(first.data)
-            first = first.next
+        for i in self:
+            if i in other:
+                new.add(i)
         return new
     
     def difference(self, other):
         new = Set()
-        first = self.head
-        while first:
-            if other.search(first.data) is None:
-                new.add(first.data)
-            first = first.next
+        for i in self:
+            if i not in other:
+                new.add(i)
         return new
 
     def subset(self, other):
-        return True if not self.difference(other).head else False
-
-
-if __name__ == '__main__':
-    a = Set()
-    a.add(1)
-    a.add(2)
-    a.add(3)
-    print(a)
-
-    b = Set()
-    b.add(3)
-    b.add(4)
-    b.add(5)
-    print(b)
-
-    print(a.union(b))
-    print(a.intersection(b))
-    print(a.difference(b))
-    print(b.difference(a))
-    print(a.subset(b))
-    print(a.subset(a))
+        return True if not self.difference(other) else False
